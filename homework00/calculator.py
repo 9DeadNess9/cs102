61ee943c9793fd052726de2ed5bd1bb5752a0824
9DeadNess9 <98608265+9DeadNess9@users.noreply.github.com>
2023-09-27 19:10:22 +0300

    calc1

--- homework00/calculator.py
+++ homework00/calculator.py
@@ -1,1 +1,49 @@
-#  PUT YOUR CODE HERE
+import math
+import typing as tp
+def calc(num_1: float, num_2: float, command: str) -> tp.Union[float, str]:
+    if command == "+":
+        return num_1 + num_2
+    if command == "-":
+        return num_1 - num_2
+    if command == "/":
+        if num_2 == 0:
+            return "Так нельзя делать"
+        return num_1 / num_2
+    if command == "*":
+        return num_1 * num_2
+    if command == "^":
+        return num_1**num_2
+    if command == "^2":
+        return num_1**2
+    if command == "sin":
+        return math.sin(math.radians(num_1))
+    if command == "cos":
+        return math.cos(math.radians(num_1))
+    if command == "tg":
+        return math.tg(math.radians(num_1))
+    if command == "ctg":
+        return math.ctg(math.radians(num_1))
+    if command == "ln":
+        if num_1 <=0:
+            return "Так нельзя делать"
+        return math.log(math.radians(num_1))
+    if command == "log10":
+        if num_1 <=0:
+            return "Так нельзя делать"
+        return math.log10(math.radians(num_1))
+    else:
+        return f"Неизвестный оператор: {command!r}."
+
+if __name__ == "__main__":
+    while True: # программа выполняется до ввода 0 вместо команды
+        COMMAND = input("Введите оперцию > ")
+        if COMMAND.isdigit() and int(COMMAND) == 0:
+            break
+        if COMMAND in ('^2', 'sin', 'cos','tg', 'ctg', 'ln', 'log10'):
+            NUM_1 = float(input("Первое число > "))
+            result = calc(NUM_1, 0, COMMAND)
+        else:
+            NUM_1 = float(input("Первое число > "))
+            NUM_2 = float(input("Второе число > "))
+            result = calc(NUM_1, NUM_2, COMMAND)
+        print(result)
