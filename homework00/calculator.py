dcf7a36fce96cab7edaf2cdd6bd6b55701f3eb22
9DeadNess9 <98608265+9DeadNess9@users.noreply.github.com>
2023-09-27 20:12:34 +0300

    updates

--- homework00/calculator.py
+++ homework00/calculator.py
@@ -1,5 +1,7 @@
 import math
 import typing as tp
+
+
 def calc(num_1: float, num_2: float, command: str) -> tp.Union[float, str]:
     if command == "+":
         return num_1 + num_2
@@ -24,22 +26,23 @@ def calc(num_1: float, num_2: float, command: str) -> tp.Union[float, str]:
     if command == "ctg":
         return math.ctg(math.radians(num_1))
     if command == "ln":
-        if num_1 <=0:
+        if num_1 <= 0:
             return "Так нельзя делать"
         return math.log(math.radians(num_1))
     if command == "log10":
-        if num_1 <=0:
+        if num_1 <= 0:
             return "Так нельзя делать"
         return math.log10(math.radians(num_1))
     else:
         return f"Неизвестный оператор: {command!r}."
 
+
 if __name__ == "__main__":
-    while True: # программа выполняется до ввода 0 вместо команды
+    while True:  # программа выполняется до ввода 0 вместо команды
         COMMAND = input("Введите оперцию > ")
         if COMMAND.isdigit() and int(COMMAND) == 0:
             break
-        if COMMAND in ('^2', 'sin', 'cos','tg', 'ctg', 'ln', 'log10'):
+        if COMMAND in ("^2", "sin", "cos", "tg", "ctg", "ln", "log10"):
             NUM_1 = float(input("Первое число > "))
             result = calc(NUM_1, 0, COMMAND)
         else:
