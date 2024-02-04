50d3d3ae4f7768a49341161b2be6857d620f7cac
9DeadNess9 <98608265+9DeadNess9@users.noreply.github.com>
2024-02-04 22:44:00 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,28 +1,50 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    """
-    Encrypts plaintext using a Vigenere cipher.
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
-    """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    shift = 0
+    keyword_i = 0
+    for cur_char in plaintext:
+        char_ord = ord(cur_char)
+        char_ord_keyword = ord(keyword[keyword_i])
+        if ord("A") <= char_ord_keyword <= ord("Z"):
+            shift = char_ord_keyword % ord("A")
+        if ord("a") <= char_ord_keyword <= ord("z"):
+            shift = char_ord_keyword % ord("a")
+        if ord("A") <= char_ord <= ord("Z"):
+            char_ord += shift
+            if char_ord > ord("Z"):
+                char_ord = ord("A") + char_ord % ord("Z") - 1
+        if ord("a") <= char_ord <= ord("z"):
+            char_ord += shift
+            if char_ord > ord("z"):
+                char_ord = ord("a") + char_ord % ord("z") - 1
+        ciphertext += chr(char_ord)
+        keyword_i += 1
+        if keyword_i == len(keyword):
+            keyword_i = 0
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    """
-    Decrypts a ciphertext using a Vigenere cipher.
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
-    """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    shift = 0
+    keyword_i = 0
+    for cur_char in ciphertext:
+        char_ord = ord(cur_char)
+        char_ord_keyword = ord(keyword[keyword_i])
+        if ord("A") <= char_ord_keyword <= ord("Z"):
+            shift = char_ord_keyword % ord("A")
+        if ord("a") <= char_ord_keyword <= ord("z"):
+            shift = char_ord_keyword % ord("a")
+        if ord("A") <= char_ord <= ord("Z"):
+            char_ord -= shift
+            if char_ord < ord("A"):
+                char_ord = ord("Z") - ord("A") % char_ord + 1
+        if ord("a") <= char_ord <= ord("z"):
+            char_ord -= shift
+            if char_ord < ord("a"):
+                char_ord = ord("z") - ord("a") % char_ord + 1
+        plaintext += chr(char_ord)
+        keyword_i += 1
+        if keyword_i == len(keyword):
+            keyword_i = 0
     return plaintext
