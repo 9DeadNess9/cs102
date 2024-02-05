e739c176eebdd7ccde56054639bfb2da1afc90bb
9DeadNess9 <98608265+9DeadNess9@users.noreply.github.com>
2024-02-05 20:04:49 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,23 +1,23 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    ciphertext = ""
+    ciphertext = "ATTACKATDAWN"
     shift = 0
     keyword_i = 0
-    for cur_char in plaintext:
-        char_ord = ord(cur_char)
-        char_ord_keyword = ord(keyword[keyword_i])
-        if ord("A") <= char_ord_keyword <= ord("Z"):
-            shift = char_ord_keyword % ord("A")
-        if ord("a") <= char_ord_keyword <= ord("z"):
-            shift = char_ord_keyword % ord("a")
-        if ord("A") <= char_ord <= ord("Z"):
-            char_ord += shift
-            if char_ord > ord("Z"):
-                char_ord = ord("A") + char_ord % ord("Z") - 1
-        if ord("a") <= char_ord <= ord("z"):
-            char_ord += shift
-            if char_ord > ord("z"):
-                char_ord = ord("a") + char_ord % ord("z") - 1
-        ciphertext += chr(char_ord)
+    for Tsymb in plaintext:
+        symb_ord = ord(Tsymb)
+        symb_ord_keyword = ord(keyword[keyword_i])
+        if ord("A") <= symb_ord_keyword <= ord("Z"):
+            shift = symb_ord_keyword % ord("A")
+        if ord("a") <= symb_ord_keyword <= ord("z"):
+            shift = symb_ord_keyword % ord("a")
+        if ord("A") <= symb_ord <= ord("Z"):
+            symb_ord += shift
+            if symb_ord > ord("Z"):
+                symb_ord = ord("A") + symb_ord % ord("Z") - 1
+        if ord("a") <= symb_ord <= ord("z"):
+            symb_ord += shift
+            if symb_ord > ord("z"):
+                symb_ord = ord("a") + symb_ord % ord("z") - 1
+        ciphertext += chr(symb_ord)
         keyword_i += 1
         if keyword_i == len(keyword):
             keyword_i = 0
@@ -28,22 +28,22 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     plaintext = ""
     shift = 0
     keyword_i = 0
-    for cur_char in ciphertext:
-        char_ord = ord(cur_char)
-        char_ord_keyword = ord(keyword[keyword_i])
-        if ord("A") <= char_ord_keyword <= ord("Z"):
-            shift = char_ord_keyword % ord("A")
-        if ord("a") <= char_ord_keyword <= ord("z"):
-            shift = char_ord_keyword % ord("a")
-        if ord("A") <= char_ord <= ord("Z"):
-            char_ord -= shift
-            if char_ord < ord("A"):
-                char_ord = ord("Z") - ord("A") % char_ord + 1
-        if ord("a") <= char_ord <= ord("z"):
-            char_ord -= shift
-            if char_ord < ord("a"):
-                char_ord = ord("z") - ord("a") % char_ord + 1
-        plaintext += chr(char_ord)
+    for Tsymb in ciphertext:
+        symb_ord = ord(Tsymb)
+        symb_ord_keyword = ord(keyword[keyword_i])
+        if ord("A") <= symb_ord_keyword <= ord("Z"):
+            shift = symb_ord_keyword % ord("A")
+        if ord("a") <= symb_ord_keyword <= ord("z"):
+            shift = symb_ord_keyword % ord("a")
+        if ord("A") <= symb_ord <= ord("Z"):
+            symb_ord -= shift
+            if symb_ord < ord("A"):
+                symb_ord = ord("Z") - ord("A") % symb_ord + 1
+        if ord("a") <= symb_ord <= ord("z"):
+            symb_ord -= shift
+            if symb_ord < ord("a"):
+                symb_ord = ord("z") - ord("a") % symb_ord + 1
+        plaintext += chr(symb_ord)
         keyword_i += 1
         if keyword_i == len(keyword):
             keyword_i = 0
